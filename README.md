f33aeb3d5b6f18d46f0a0891284f0d3b21c4b3fb
vitosaveroaw <vitosaveroaw@gmail.com>
2026-09-16 02:39:54 -0400

    Load california housing dataset
    Add requirements.txt, create src and figures folder, create the python file, update the readme
    
    Python file just consist load dataset at the moment

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # A02-yah25002-sm21003
 A02 Assignment
+Ping pong assignment
+
+
+## How to run the code:
+```pip install -r requirements.txt```
+```python src/a02.py```
---
+++ src/a02.py
@@ -0,0 +1,12 @@
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+
+# Load California Housing dataset
+housing = fetch_california_housing(as_frame=True)
+
+# Features + target as a single DataFrame
+df = housing.frame
+
+# Quick check
+print(df.head())
+print(df.shape)
